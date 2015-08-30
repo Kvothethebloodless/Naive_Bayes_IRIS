3b4e5b97b76b86d6de694a78431f51ff29df9a9d
Manish <manish97ravula@gmail.com>
2015-08-30 16:12:46 +0530

    Inference for Naive working properly

--- main.py
+++ main.py
@@ -28,26 +28,56 @@ def norm_pdf_multivariate(x, param):
         result = math.pow(math.e, -0.5 * (x_mu * inv * x_mu.T))
         return norm_const * result
     else:
-        raise NameError("The dimensions of the input don't match")
+        raise NameError("The dimensions of the inputs don't match")
     
     
-def inference_prior(iris_traindata):
+def inference_prior(traindata):
     C_is = np.array([0,1,2])
-    freq_Cis = np.array([np.sum(iris_traindata[:,4]==C_i)  for C_i in C_is])
+    freq_Cis = np.array([np.sum(traindata[:,4]==C_i)  for C_i in C_is])
     prior_Cis =  freq_Cis/(np.sum(freq_Cis))
     return np.array([C_is,prior_Cis])
     
 
-def decision_posterior(Ci_params,datapoint,n):
-    #Ci_params is in accordance with [Ci,[mu_matrix,var_matrix]] given data point, calculate the posterior probabilities
+def decision_posterior(Ci_params,datapoints,n_classes,n_datapoints):
+    #Ci_params is a tuple with (class_labels,mu_array,var_matrix) as elements
+    # Given n dimension data with I classes - size of Ci_params is 3 by 
+    #given data point, calculate the posterior probabilities
     #and put it back with [Ci,posterior]
     #Return Ci with maximum posterior probability.
-    posterior_prob = np.zeros((3,2));
-    posterior_prob[:,0] = Ci_params[:,0]; #Writing classes
-    posterior_prob[:,1] = norm_pdf_multivariate(datapoint,Ci_params[:,1]);
-    max_post_prob_loc = np.argmax(posterior_prob[1]);                                  ;
-    max_post_class = posterior_prob[:,0][max_post_prob_loc]
+    (class_labels,mu_array,var_matrix) = Ci_params;
+    posterior_prob = np.zeros((n_classes,2));
+    posterior_prob[:,0] = class_labels; #Writing classes
+    decided_class=np.arange(n_datapoints);
+    for j in range(n_datapoints):
+        datapoint =datapoints[j];
+        for i in range(n_classes):
+            posterior_prob[i,1] = norm_pdf_multivariate(datapoint,(mu_array[i],var_matrix[i]));
+        max_post_prob_loc = np.argmax(posterior_prob[:,1]);                                  
+        max_post_class = posterior_prob[:,0][max_post_prob_loc]
+        decided_class[j] = max_post_class;
+    return decided_class
+
+def inference_likelihood(traindata,class_labels,n_classes,n_features): #Naive Bayes, Class Conditional Independence assumption + MLE estimation
+    #return a matrix with parameters for each class.
+    #return a tuple with 3 nparrays - class_labels,mu_array,var_matrix
     
+    all_class_mu = np.zeros((n_classes,n_features));
+    all_class_var = np.zeros((n_classes,n_features,n_features));
+    
+    for i in range(n_classes):
+        data_ci = traindata[np.where((traindata[:,4]==class_labels[i]))];
+        data_ci = data_ci[:,0:4];
+        mu_array = np.mean(data_ci,axis=0); #Maximum Likelihood
+        var_array = np.var(data_ci,axis=0); #Maximum Likelihood
+        var_matrix = np.diag(var_array);
+        all_class_mu[i] = mu_array;
+        all_class_var[i] = var_matrix;
+    all_class_params = (class_labels,all_class_mu,all_class_var) ;
+    return all_class_params
+
+        
+        
+
 
     
     
@@ -63,8 +93,9 @@ def decision_posterior(Ci_params,datapoint,n):
 
 
 if __name__=='main':
-    [iris_traindata,iris_testdata] = pi.get_data();
-    no_classes = 3
+    [traindata,testdata] = pi.get_data();
+    iris_class_data = np.zeros(no_classes,40) #40 samples per class
+    
 
 
 
