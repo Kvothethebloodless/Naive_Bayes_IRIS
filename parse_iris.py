8b7620e376300eff4c2cafdba302d42988fc6b4d
Manish <manish97ravula@gmail.com>
2015-08-30 12:17:00 +0530

    Added a function to seperately deliver training and test data of size 40 and 10

--- parse_iris.py
+++ parse_iris.py
@@ -5,7 +5,7 @@ def parse_iris():
         data_newlinesplit = [st.split(',') for st in open('IRIS.txt').readlines()]
         data_newlinesplit = data_newlinesplit[0:150]
         for i in range(150):
-                print(i)
+               
                 if data_newlinesplit[i][4] == 'Iris-setosa\n':
                         data_newlinesplit[i][4] = 0
                 elif data_newlinesplit[i][4] == 'Iris-versicolor\n':
@@ -18,7 +18,14 @@ def parse_iris():
                 
         data_np= np.array(data_newlinesplit);
         return data_np
-
-
+def get_data():
+        iris_data = parse_iris();
+        locs = np.arange(40)
+        locs2 = np.arange(10)
+        trainelems_loc= np.concatenate([locs,locs+50,locs+100])
+        testelems_loc = np.concatenate([locs2+40,locs2+90,locs2+140])
+        iris_traindata = iris_data[trainelems_loc]
+        iris_testdata = iris_data[testelems_loc]
+        return([iris_traindata,iris_testdata])   
 
    
